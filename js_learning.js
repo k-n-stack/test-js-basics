bf83d27289bd6d8066ec9a58c9cae963f645c572
nico-php-coder <khin.nicolas@gmail.com>
2021-06-07 20:55:25 +0200

    ok for first seven algorithms

--- js_learning.js
+++ js_learning.js
@@ -10,7 +10,7 @@ const output01 = document.querySelector("#output-algo-01");
 
 // choose a random integer number between 1 and 100
 let randomNumber = Math.floor(Math.random() * 100 + 1);
-let round = 0;
+let round01 = 0;
 
 console.log("algo01 answer : " + randomNumber);
 
@@ -27,15 +27,15 @@ function output_function_01(guessNumber) {
         return "Please, enter a valid value";
     }
     if(intNumber > randomNumber) {
-        ++round;
-        return "round " + round + ": " + "The number provided is too high";
+        ++round01;
+        return "round " + round01 + ": " + "The number provided is too high";
     }
     if(intNumber < randomNumber) {
-        ++round;
-        return "round " + round + ": " + "The number provided is too low";
+        ++round01;
+        return "round " + round01 + ": " + "The number provided is too low";
     }
     if(intNumber == randomNumber) {
-        return "round " + round + ": " + "Congratulation, you found the number";
+        return "round " + round01 + ": " + "Congratulation, you found the number";
     }
 }
 
@@ -319,7 +319,7 @@ const div05 = document.getElementById('div-algo-05');
  * @param {*} event 
  */
 function input_function_05(event) {
-    let colorModifier = 255 - ((input05.value.length < 10) ? Math.floor((input05.value.length * 255) / 10) : 255);
+    let colorModifier = 255 - ((input05.value.length < 30) ? Math.floor((input05.value.length * 255) / 30) : 255);
     console.log(colorModifier);
     div05.style.backgroundColor = `rgb(255, ${colorModifier}, 255)`;
 }
@@ -388,3 +388,181 @@ function btn06_onClick(event) {
 
 // when user click on button...
 button06.addEventListener('click', btn06_onClick);
+
+/****************************************
+ * algo 07
+ * User enter a number.
+ * Script randomly choose a number to guess user number.
+ * on each round, user have to tell if previously number is too high of too low
+ * after 5 round, Script loose if number is not guessed.
+ * 
+ ****************************************/
+
+const input07 = document.querySelector("#input-algo-07");
+const button07 = document.querySelector("#button-algo-07");
+const output07 = document.querySelector("#output-algo-07");
+const form07 = document.querySelector("#form-algo-07");
+const list07 = document.querySelector("#list-algo-07");
+const main07 = document.querySelector("#algo-07");
+
+let userInputNumber;
+let round07 = 50;
+let scriptMin = 1;
+let scriptMax = 100;
+let randNumber;
+
+/**
+ * when user provide valid number, change form to remove
+ * input field and add "greater" and "smaller" button
+ */
+function change_form() {
+    
+    form07.querySelectorAll('*').forEach(value => value.remove()); 
+    
+    let greaterButton = document.createElement("button");
+    let greaterButtonText = document.createTextNode("Greater");
+    greaterButton.appendChild(greaterButtonText);
+    greaterButton.setAttribute('id', 'greater-button');
+    
+    let smallerButton = document.createElement("button");
+    let smallerButtonText = document.createTextNode("Smaller");
+    smallerButton.appendChild(smallerButtonText);
+    smallerButton.setAttribute('id', 'smaller-button');
+    
+    form07.appendChild(greaterButton);
+    form07.appendChild(smallerButton);
+    
+}
+
+/**
+ * add event listener to "Greater" and "Smaller" buttons
+ */
+function addGreaterSmallerAL() {
+    
+    let greater = document.querySelector("#greater-button");
+    let smaller = document.querySelector("#smaller-button");
+    greater.addEventListener('click', greater_onClick);
+    smaller.addEventListener('click', smaller_onClick);
+
+}
+
+/**
+ * if greater is clicked, set the search range min to guess value
+ * @param {*} value 
+ */
+function greater_onClick(event) {
+
+    event.preventDefault();
+
+    scriptMin = randNumber + 1;
+
+    guess();
+
+}
+
+/**
+ * if smaller is clicked, set the search range max to guess value
+ * @param {*} value 
+ */
+function smaller_onClick(event) {
+
+    event.preventDefault();
+
+    scriptMax = randNumber - 1;
+
+    guess();
+
+}
+
+/**
+ * from mdn, get int random between min and max, both inclusive
+ * @param {*} min 
+ * @param {*} max 
+ * @returns 
+ */
+function getRandomIntInclusive(min, max) {
+
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min +1)) + min;
+
+}
+
+/**
+ * return a <li> element with random number within range 
+ * depending if script win, has round left, etc...
+ */
+function guess() {
+
+    if(round07 > 0) {
+
+        randNumber = getRandomIntInclusive(scriptMin, scriptMax);
+
+        let listElem = document.createElement("li");
+        let listElemText = document.createTextNode("I think your number is " + randNumber);
+        listElem.appendChild(listElemText);
+        list07.appendChild(listElem);
+
+        if(randNumber == userInputNumber) {
+
+            let winDiv = document.createElement("div");
+            let winDivText = document.createTextNode("Gotcha ! >=)");
+            winDiv.appendChild(winDivText);
+            main07.appendChild(winDiv);
+
+            form07.querySelectorAll('*').forEach(value => value.remove());
+
+        }
+
+    } else {
+
+        let looseDiv = document.createElement("div");
+        let looseDivText = document.createTextNode("I loose, no turn left =(");
+        looseDiv.appendChild(looseDivText);
+        main07.appendChild(looseDiv);
+
+        form07.querySelectorAll('*').forEach(value => value.remove());
+
+    }
+
+    --round07;
+ 
+}
+
+/**
+ * return output pharse or value depends on user input
+ * @param {*} number 
+ * @returns 
+ */
+function output_function_07(number) {
+
+    intNumber = parseInt(number);
+
+    if(isNaN(intNumber)) {
+        return "Please, provide a valid value.";
+    }
+    if(intNumber < 1 || intNumber > 100) {
+        return "Please, provide a number between 1 and 100.";
+    }
+
+    userInputNumber = intNumber;
+    change_form();
+    addGreaterSmallerAL();
+    return "You provided number " + intNumber;
+
+}
+
+/**
+ * handle event when user click on button
+ * @param {*} event 
+ */
+function btn07_onClick(event) {
+
+    event.preventDefault();
+    output07.textContent = output_function_07(input07.value);
+    guess();
+
+}
+
+// when user click on save button...
+button07.addEventListener('click', btn07_onClick);
